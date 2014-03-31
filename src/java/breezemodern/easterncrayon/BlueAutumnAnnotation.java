7f5d8f778103e5e384cfa492798a7016a1ed7229
Emory Merryman <emory.merryman@gmail.com>
2014-03-31 11:47:06 -0400

    fixed some stuff

--- src/java/breezemodern/easterncrayon/BlueAutumnAnnotation.java
+++ src/java/breezemodern/easterncrayon/BlueAutumnAnnotation.java
@@ -30,9 +30,13 @@ import java . lang . annotation . Documented ;
 import java . lang . annotation . Inherited ;
 import java . lang . annotation . Retention ;
 import java . lang . annotation . Target ;
+
 import static java . lang . annotation . ElementType . TYPE ;
 import static java . lang . annotation . RetentionPolicy . SOURCE ;
 
+/**
+ * For the BlueAutumnProcessor (for functional-tests).
+ **/
 @ Documented
     @ Inherited
     @ Retention ( TYPE )
