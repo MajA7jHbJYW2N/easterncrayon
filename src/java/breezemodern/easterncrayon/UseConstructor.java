7139e53702c1234eca302cd253d10293b2d33d5d
Emory Merryman <emory.merryman@gmail.com>
2014-04-08 08:31:59 -0400

    one more annotation

---
+++ src/java/breezemodern/easterncrayon/UseConstructor.java
@@ -0,0 +1,41 @@
+///////////////////////////////////////////////////////////////////////
+//
+// Copyright © (C) 2014
+// Emory Hughes Merryman, III
+// emory.merryman@gmail.com
+//
+// This file is part of easterncrayon.
+//
+// easterncrayon is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// easterncrayon is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with easterncrayon.  If not, see <http://www.gnu.org/licenses/>.
+//
+///////////////////////////////////////////////////////////////////////
+
+package breezemodern . easterncrayon ;
+
+import java . lang . annotation . Documented ;
+import java . lang . annotation . Inherited ;
+import java . lang . annotation . Retention ;
+import java . lang . annotation . Target ;
+
+import static java . lang . annotation . ElementType . METHOD ;
+import static java . lang . annotation . RetentionPolicy . RUNTIME ;
+
+@ Documented
+    @ Inherited
+    @ Retention ( RUNTIME )
+    @ Target ( METHOD )
+    public @ interface UseConstructor
+	     {
+		 Class < ? > value ( ) ;
+    }
